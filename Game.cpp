f387f0fa8b90b5b234937ead7853da6f5407dad5
ajhanson22227 <ajhanson22227@gmail.com>
2020-08-03 20:49:25 -0500

    Add comments

--- Game.cpp
+++ Game.cpp
@@ -3,21 +3,29 @@
 
 Game::Game(sf::VideoMode videoMode, float padWidth, float padHeight, float ballSide){
     window.create(videoMode, "BRICK BREAKER", sf::Style::Default);
+    //sets the paddle position to half the window and to the bottom of the screen
     paddle = Paddle(sf::Vector2f(padWidth, padHeight), videoMode.width / 2 - padWidth, videoMode.height - padHeight - 10);
+    //Set ball position to half the winow width and puts it in the air so it doesn't hit the paddle right away
     ball = Ball(sf::Vector2f(ballSide, ballSide), videoMode.width / 2 - ballSide, videoMode.height - 4*ballSide);
 }
 
 Game::~Game(){}
 
 void Game::update(){
+    //move paddle
     paddle.update();
 
+    //Get ball interactions
+    //If the ball hits paddle
     if (ball.getPosition().intersects(paddle.getPosition()))
         ball.hitPaddle();
+    //if the ball hits the left/right side
     if (ball.getPosition().left == 0 || ball.getPosition().left + 50 > 800)
         ball.hitSide();
+    //if the ball hits the top of screen
     if (ball.getPosition().top == 0)
         ball.hitTop();
+    //if the ball hits the bottom of screen
     if (ball.getPosition().top == window.getSize().y)
         ball.hitBottom();
     ball.update();
